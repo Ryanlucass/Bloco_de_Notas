ade342be527a850e6c5796bd1ac5fdef1889f5a5
Lucas alves <lucasryanalves@gmail.com>
2021-01-15 09:59:19 -0300

    update
    Incializando

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bloco_de_Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
